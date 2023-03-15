a92c2cab56d242ef0863ce436fe39b0196ada3f3
ghkdrmatks <jewon1202@gmail.com>
2023-03-15 23:43:35 +0900

    3

--- jmc/urls.py
+++ jmc/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get/restaurant', views.getRestaurant, name="getRestaurant"),
-    path('get/menu/<int:restaurant>', views.getMenuByRestaurant, name="getMenubyRestaurant"),
-    path('get/menu', views.getMenu, name="getMenu"),
+    path('restaurant', views.getRestaurant, name="getRestaurant"),
+    path('menu/<int:restaurant>', views.getMenuByRestaurant, name="getMenubyRestaurant"),
+    path('menu', views.getMenu, name="getMenu"),
 ]
