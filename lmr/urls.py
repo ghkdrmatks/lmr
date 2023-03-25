208e54774e5bb35aa5234fac63ee77ecf40ba86a
ghkdrmatks <jewon1202@gmail.com>
2023-03-25 20:16:22 +0900

    imagetest4

--- lmr/urls.py
+++ lmr/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('data/', include('jmc.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
